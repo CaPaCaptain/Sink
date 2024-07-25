08d077dac63eb098d19d0234e8a9f0dffe782d28
CaPaCaptain <96831215+CaPaCaptain@users.noreply.github.com>
2024-07-25 11:21:19 +0800

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors({
+    origin:"*"
+}));
+
+app.get("/data",(req,res) => {
+  res.json({ name: "Kyle", favoriteFood: "Rice"})
+})
+
+app.listen(3000)       
